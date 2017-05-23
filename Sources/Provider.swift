6f85e379858866ef91cd0b082a6b9876269af1ba
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-05-23 15:18:15 -0400

    updated provider

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -4,43 +4,48 @@ import HTTP
 
 
 public final class Provider: Vapor.Provider {
-    
-  public static let repositoryName: String = "vapor-s3-signer"
+ 
+  public static let repositoryName: String = "vapor-S3Signer"
+  private static let configFileName: String = "vapor-S3Signer"
   
   public var s3Signer: S3SignerAWS
     
     public init(accessKey: String, secretKey: String, region: Region, securityToken: String? = nil) {
         self.s3Signer = S3SignerAWS(accessKey: accessKey, secretKey: secretKey, region: region, securityToken: securityToken)
     }
+  
+  
+  
+  public convenience init(config: Config) throws {
     
-    public convenience init(config: Config) throws {
-        guard let vaporS3 = config["vapor-S3Signer"]?.object else {
-            throw S3ProviderError.config("no vapor-S3Signer.json config file")
-        }
-        guard let accessKey = vaporS3["accessKey"]?.string else {
-            throw S3ProviderError.config("No 'accessKey' key in vapor-S3Signer.json config file.")
-        }
-        
-        guard let secretKey = vaporS3["secretKey"]?.string else {
-            throw S3ProviderError.config("No 'secretKey' key in vapor-S3Signer.json config file.")
-        }
-        
-        guard let region = vaporS3["region"]?.string else {
-            throw S3ProviderError.config("No 'region' key in vapor-S3Signer.json config file.")
-        }
-        
-        guard let regionEnum = Region(rawValue: region) else {
-            throw S3ProviderError.config("region name does not conform to any Region raw values. Check Region.swift for proper names.")
-        }
-
-        let token = vaporS3["securityToken"]?.string
-        
-        self.init(accessKey: accessKey, secretKey: secretKey, region: regionEnum, securityToken: token)
-
+    guard config[Provider.configFileName] != nil else {
+      throw S3ProviderError.config("no vapor-S3Signer.json config file")
     }
+    
+    guard let accessKey = config[Provider.configFileName, "accessKey"]?.string else {
+      throw S3ProviderError.config("No 'accessKey' key in vapor-S3Signer.json config file.")
+    }
+    
+    guard let secretKey = config[Provider.configFileName, "secretKey"]?.string else {
+      throw S3ProviderError.config("No 'secretKey' key in vapor-S3Signer.json config file.")
+    }
+    
+    guard let region = config[Provider.configFileName, "region"]?.string else {
+      throw S3ProviderError.config("No 'region' key in vapor-S3Signer.json config file.")
+    }
+    
+    guard let regionEnum = Region(rawValue: region) else {
+      throw S3ProviderError.config("region name does not conform to any Region raw values. Check Region.swift for proper names.")
+    }
+    
+    let token = config[Provider.configFileName, "securityToken"]?.string
+    
+    self.init(accessKey: accessKey, secretKey: secretKey, region: regionEnum, securityToken: token)
+    
+  }
   
   
-  public func boot(_ config: Config) throws {}
+  public func boot(_ config: Config) throws { }
   
   public func boot(_ droplet: Droplet) throws {
     droplet.storage["s3Signer"] = self.s3Signer
