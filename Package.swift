b1611aefb4f864e613b6c031fb6c4c47a237a95e
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-09-20 02:32:02 -0400

    Update s3SignerAWS to version 3

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/JustinM1/S3SignerAWS.git",
-                 majorVersion: 2),
+                 majorVersion: 3),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ]
 )
--- Tests/VaporS3SignerTests/VaporS3SignerTests.swift
+++ Tests/VaporS3SignerTests/VaporS3SignerTests.swift
@@ -1,22 +1,36 @@
-import XCTest
 import Vapor
-
+import XCTest
 @testable import VaporS3Signer
 
 class VaporS3SignerTests: XCTestCase {
-    func testDropletExtension() {
-        let drop = Droplet()
-        drop.addProvider(VaporS3Signer.Provider(accessKey: "access", secretKey: "secret", region: .usWest1, securityToken: "token"))
-        if let signer = drop.s3Signer {
-            XCTAssert(signer.region == .usWest1)
-        } else {
-            XCTAssert(false)
-        }
-    }
+	
+	static var allTests : [(String, (VaporS3SignerTests) -> () throws -> Void)] {
+		return [
+			("testDropletExtension", testDropletExtension)
+		]
+	}
+	
+	func testDropletExtension() {
+		do {
+			let config = try Config()
+			
+			try config.addProvider(VaporS3Signer.Provider(
+				accessKey: "access",
+				secretKey: "secret",
+				region: .usWest1,
+				securityToken: "token"))
+			
+			let drop = try Droplet(config)
+			
+			guard let signer = drop.s3Signer else {
+				XCTFail("Droplet s3Signer should not be nil.")
+				return
+			}
+			
+			XCTAssertEqual(signer.region, .usWest1)
 
-    static var allTests : [(String, (VaporS3SignerTests) -> () throws -> Void)] {
-        return [
-            ("testDropletExtension", testDropletExtension)
-        ]
+		} catch {
+			XCTFail("Error caught during test: \(error)")
+		}
     }
 }
