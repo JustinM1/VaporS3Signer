15a94c6b3f31744b58000c6af9ce7cbbae34c2d4
JustinM <justin.moehringer@Justins-iMac.fios-router.home>
2017-10-04 01:04:40 -0400

    corrected vaporHeaders access level.

--- Sources/Dictionary+vaporHeaders.swift
+++ Sources/Dictionary+vaporHeaders.swift
@@ -3,7 +3,7 @@ import HTTP
 
 extension Dictionary where Key == String, Value == String {
 	
-	var vaporHeaders: [HeaderKey: String] {
+	public var vaporHeaders: [HeaderKey: String] {
 		var newHeaders: [HeaderKey: String] = [:]
 		self.forEach { newHeaders.updateValue($0.value, forKey: HeaderKey($0.key)) }
 		return newHeaders
